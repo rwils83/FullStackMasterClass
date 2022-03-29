96b2f43784b9960ccb2f87b3c42be1a0df3418b2
Ryan Wilson <rwils83@gmail.com>
2022-03-29 17:48:34 -0400

    Adding python lessons

---
+++ Section_7_python_advanced/error_and_exception.py
@@ -0,0 +1,13 @@
+try:
+    print("10"+10)
+except IOError:
+    print("You have an input output error")
+    print("Did you check file permissions?")
+except TypeError:
+    print("You used the wrong data type somewhere you fuck")
+except:
+    print("You got an error")
+else:
+    print("Else block ran")
+
+
